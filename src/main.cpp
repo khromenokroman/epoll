9c1c68c8507a57fed3e46123a3b73ea3ca90c188
khromenokroman <roma55592@yandex.ru>
2023-05-28 00:18:21 +0200

    fail

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    struct pollfd fds[2]; // инициализация структуры
+    struct pollfd fds; // инициализация структуры
 
     int fd_in, fd_out;         // файловые дескрипторы
     int result;                // готовность дескрипторов
@@ -15,8 +15,8 @@ int main()
     file_name_in = "input.txt"; // имена файлов
     file_name_out = "output.txt";
 
-    fd_in = open(file_name_in, O_RDONLY | O_NONBLOCK, 0770); // открывам файл источник, только чтение, неблокирующий. rwxrwx---
-    if (fd_in == -1)                                         // проверим на ошибку открытия
+    fd_in = open(file_name_in, O_RDONLY, 0770); // открывам файл источник, только чтение, неблокирующий. rwxrwx---
+    if (fd_in == -1)                            // проверим на ошибку открытия
     {
         printf("Не могу открыть файл источник!\n");
         return -1;
@@ -28,17 +28,27 @@ int main()
         return -1;
     }
 
-    fds[0].fd = fd_in;      // добавим файл источник в мониторинг
-    fds[0].events = POLLIN; // событие ввод
+    fds.fd = fd_in;      // добавим файл источник в мониторинг
+    fds.events = POLLIN; // события, происходящие с файловым дескриптором
 
-    result = poll(fds, 1, 0); // узнаем что готово
-    if (result == -1)         // проверим на ошибку открытия
+    while (true)
     {
-        printf("Не могу получить список готовности фалов!\n");
-        return -1;
+        result = poll(&fds, 1, 100); // узнаем что готово
+        if (result == -1)            // проверим на ошибку открытия
+        {
+            printf("Не могу получить список готовности фалов!\n");
+            return -1;
+        }
+        else if (result > 0)
+        {
+            if (fds.revents & POLLIN)
+            {
+                printf("что то есть!");
+                return 0;
+            }
+        }
     }
-    printf("Результат: %d\n", result);
 
-    close(fd_in);
+    close(fd_in); // чистим ресурсы
     close(fd_out);
 }
