e2942d50bc40cc594e36d95ec513ee6f84ea0a0a
khromenokroman <roma55592@yandex.ru>
2023-05-27 11:14:19 +0200

    add nonblock

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 int main()
 {
 
-    int fd_text = open("1.txt", O_RDONLY | O_NONBLOCK, 0770);
+    int fd_text = open("1.txt", O_RDONLY, 0770);
     if (fd_text == -1)
     {
         printf("Error open file!!\nNeed file 1.txt\n");
@@ -19,29 +19,32 @@ int main()
     {
         printf("file 1.txt fd: %d\n", fd_text);
         int efd = epoll_create(1);
-        if (efd ==-1)
+        if (efd == -1)
         {
             printf("Error epoll create!!!\n");
             return -1;
         }
         printf("epoll fd: %d\n", efd);
-        // struct epoll_event ev1;
-        // ev1.data.fd = fd_text;
-        // ev1.events = EPOLLIN;
-        // if (epoll_ctl(efd, EPOLL_CTL_ADD, fd_text, &ev1)) //в книге написанно что нельяз передовать дескриптор файла
-        // {
-        //     printf("Error epoll ctl!!!\n");
-        //     return -1;
-        // }
 
-        struct epoll_event ev2;
-        ev2.data.fd = 0;
-        ev2.events = EPOLLIN;
-        if (epoll_ctl(efd, EPOLL_CTL_ADD, 0, &ev2)) //в книге написанно что нельяз передовать дескриптор файла
+        fcntl(fd_text, fcntl(fd_text, F_GETFL) | O_NONBLOCK);
+
+        struct epoll_event ev1;
+        ev1.data.fd = fd_text;
+        ev1.events = EPOLLIN;
+        if (epoll_ctl(efd, EPOLL_CTL_ADD, fd_text, &ev1) == -1) // в книге написанно что нельяз передовать дескриптор файла
         {
             printf("Error epoll ctl!!!\n");
             return -1;
         }
+
+        // struct epoll_event ev2;
+        // ev2.data.fd = 0;
+        // ev2.events = EPOLLIN;
+        // if (epoll_ctl(efd, EPOLL_CTL_ADD, 0, &ev2)) //в книге написанно что нельяз передовать дескриптор файла
+        // {
+        //     printf("Error epoll ctl!!!\n");
+        //     return -1;
+        // }
 
         while (1)
         {
@@ -66,9 +69,9 @@ int main()
                 for (int i = 0; i < res; i++)
                 {
                     struct epoll_event *cur = &events[i];
-                    if (cur->data.fd == 0)
+                    if (cur->data.fd == fd_text)
                     {
-                        printf("file is add!!!\n");
+                        printf("find\n");
                         return 0;
                     }
                 }
