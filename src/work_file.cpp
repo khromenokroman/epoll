d9d143adf51492f95e09dcd36532fdd5d6256116
khromenokroman <roma55592@yandex.ru>
2023-05-28 11:44:55 +0200

    add constr

--- src/work_file.cpp
+++ src/work_file.cpp
@@ -17,4 +17,63 @@ File::File(char const *file_name_in, char const *file_name_out)
     fds.fd = fd_in;      // добавим файл источник в мониторинг
     fds.events = POLLIN; // события, происходящие с файловым дескриптором
 
+    while (true)
+    {
+        result = poll(&fds, 1, -1); // узнаем что готово
+        if (result == -1)           // проверим на ошибку
+        {
+            My_error("Не могу получить список готовности файлов!");
+        }
+        else if (result > 0)
+        {
+            if (fds.revents & POLLIN)
+            {
+                while ((num_pread = pread(fd_in, buf, SIZE_BUFFER, off_s)) > 0) // читаем пока есть что читать
+                {
+                    if (num_pread < SIZE_BUFFER) //если размер считанных данных меньше буфера
+                    {
+                        printf("Начинаем запись в файл данные меньше буфера\n");
+                        size_t bytes_to_write = num_pread;
+                        for (int bytes_written = 0; bytes_written < bytes_to_write;) // проверим
+                        {
+
+                            int currently_written = write(fd_out, buf + bytes_written, bytes_to_write - bytes_written); // запишем в файл
+                            if (currently_written == -1)                                                                
+                            {
+                                My_error("Не могу записать в целевой файл!!");
+                            }
+
+                            bytes_written += currently_written;
+                        }
+                        printf("Запись завершена данные меньше буфера\n");
+                    }
+                    else //если больше
+                    {
+                        printf("Начинаем запись в файл данные больше буфера\n");
+                        for (; num_pread != 0; num_pread - SIZE_BUFFER)
+                        {
+                            size_t bytes_to_write = SIZE_BUFFER;
+                            for (int bytes_written = 0; bytes_written < bytes_to_write;) // проверим
+                            {
+
+                                int currently_written = write(fd_out, buf + bytes_written, bytes_to_write - bytes_written); // запишем в файл
+                                if (currently_written == -1)                                                              
+                                {
+                                    My_error("Не могу записать в целевой файл!!\n");
+                                }
+
+                                bytes_written += currently_written;
+                            }
+                            num_pread -= SIZE_BUFFER;
+                        }
+                        printf("Запись завершена данные больше буфера\n");
+                    }
+                    off_s += num_pread; //проитерировал смещение
+                    printf("Позиция курсора: %ld\n", off_s);
+                }
+            }
+        }
+        // return 0;
+    }
+
 }
