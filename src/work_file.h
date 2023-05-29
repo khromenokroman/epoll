e049bcffad447a658e77dcf6a0b71e44355990b5
khromenokroman <roma55592@yandex.ru>
2023-05-29 12:04:33 +0200

    добавил enum

--- src/work_file.h
+++ src/work_file.h
@@ -10,16 +10,21 @@
 class File final
 {
 private:
-    struct pollfd fds;                      // инициализация структуры poll
-    int fd_in = -1, fd_out = -1;            // файловые дескрипторы
-    ssize_t num_pread;                      // сколько байт причали
-    int result;                             // готовность дескрипторов
-    off_t off_s;                            // смещение источника
+    enum Type_oerations
+    {
+        wr,
+        rd
+    };                                     // операции с файлом
+    struct pollfd fds;                     // инициализация структуры poll
+    int fd_in = -1, fd_out = -1;           // файловые дескрипторы
+    ssize_t num_pread;                     // сколько байт причали
+    int result;                            // готовность дескрипторов
+    off_t off_s;                           // смещение источника
     static const size_t SIZE_BUFFER = 100; // размер буфера
-    std::unique_ptr<char[]> buf;            // буфер в динамической памяти
-    bool status = false;                    // признак того что надо ли запускать мониторинг
+    std::unique_ptr<char[]> buf;           // буфер в динамической памяти
+    bool status = false;                   // признак того что надо ли запускать мониторинг
 
-    explicit File(const char *file_input);  // конструктор основной
+    File(const char *file_input);           // конструктор основной
     void start();                           // запустить мониторинг
     bool stop();                            // остановить мониторинг
     void write_file(size_t bytes_to_write); // записать данные в файл
